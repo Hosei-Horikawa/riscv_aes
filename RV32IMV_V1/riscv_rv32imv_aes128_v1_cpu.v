fdca944fa4c80fe1da159062dd3f1db21f3e6566
Horikawa <kiohasa@gmail.com>
2025-02-18 14:20:34 +0900

    Update riscv_rv32imv_aes128_v1_cpu.v

--- RV32IMV_V1/riscv_rv32imv_aes128_v1_cpu.v
+++ RV32IMV_V1/riscv_rv32imv_aes128_v1_cpu.v
@@ -221,7 +221,7 @@ module riscv_rv32imv_aes128_v1_cpu (clk,clrn,inst,mem,pc,alu_out,b,wmem,vector);
         end else begin
             if (wreg && |rd) 
                 regfile[rd] <= data_2_rf;                  // write port
-				if (wpc) begin
+	    if (wpc) begin
                 // write port
                 if (we[0]) vregfile[vd][ 31:  0] <= v_data_2_rf[ 31:  0];
                 if (we[1]) vregfile[vd][ 63: 32] <= v_data_2_rf[ 63: 32];
@@ -232,7 +232,7 @@ module riscv_rv32imv_aes128_v1_cpu (clk,clrn,inst,mem,pc,alu_out,b,wmem,vector);
     end
     
     // for AES
-    wire        ark = v_vaddrkvx;
+    wire            ark = v_vaddrkvx;
     // Encrypt (0) or decrypt (1)
     wire            dec = 1 & (v_vinvsubbytesv | v_vinvshiftrowsv | v_vinvmixcolumnsv);
     wire            subbytes = v_vsubbytesv | v_vinvsubbytesv;
@@ -387,7 +387,7 @@ module riscv_rv32imv_aes128_v1_cpu (clk,clrn,inst,mem,pc,alu_out,b,wmem,vector);
     aes_v1 v1(ark,a,subbytes,shiftrows,mixcolumns,vb,dec,aes_vc);
     
     // control signals, will be combinational circuit
-    always @(*) begin                                      // 38 instructions
+    always @(*) begin
         alu_out = 0;                                       // alu output
         mem_out = 0;                                       // mem output
         m_addr  = 0;                                       // memory address
@@ -402,8 +402,8 @@ module riscv_rv32imv_aes128_v1_cpu (clk,clrn,inst,mem,pc,alu_out,b,wmem,vector);
         vta   = 0;
         vma   = 0; 
         v_alu_out = 0;                                     // alu output for vector
-  		    v_mem_out = 0; 
-  		    vector   = 0;
+  	v_mem_out = 0; 
+  	vector   = 0;
         case (1'b1)
             i_add: begin                                   // add
                 alu_out = a + b32;
@@ -591,8 +591,8 @@ module riscv_rv32imv_aes128_v1_cpu (clk,clrn,inst,mem,pc,alu_out,b,wmem,vector);
               vsew  [2:0] = zimm[5:3];
               vta         = zimm[6];
               vma         = zimm[7]; 
-				      alu_out = d;
-				      wreg = 1;end
+	      alu_out = d;
+	      wreg = 1;end
             v_vle32: begin
               alu_out  = {ap12,ap8,ap4,a};
               m_addr  = {alu_out[31:2] ,2'b00};        // alu_outX[1:0] != 0, exception
@@ -600,7 +600,7 @@ module riscv_rv32imv_aes128_v1_cpu (clk,clrn,inst,mem,pc,alu_out,b,wmem,vector);
               v_mem_out = {mem};
               wpc    = 1;
               vector = 1;
-				    end
+	    end
             v_vse32: begin
               alu_out  = {ap12,ap8,ap4,a};
               m_addr  = {alu_out[31:2] ,2'b00};        // alu_outX[1:0] != 0, exception
